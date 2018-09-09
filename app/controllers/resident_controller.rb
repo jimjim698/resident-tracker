c0fed1eadbc8ac82d852a44f10c90969d7778805
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-09 01:00:32 +0000

    add residents

--- app/controllers/resident_controller.rb
+++ app/controllers/resident_controller.rb
@@ -2,6 +2,12 @@ class ResidentController < ApplicationController
 
   get '/residents/new' do
     erb :'/residents/add_resident'
+  end
+
+  post '/residents/new' do
+    @resident = Resident.create(params[:resident])
+    @resident.manager_id = session[:user_id]
+    redirect "/managers/#{session[:user_id]}"
   end 
 
 end
