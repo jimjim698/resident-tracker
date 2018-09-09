4bac9719c7e41260a86c5763c78284dd6fe4f1b1
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-09 03:35:54 +0000

    cannot add sanction with empty name

--- app/controllers/sanction_controller.rb
+++ app/controllers/sanction_controller.rb
@@ -8,14 +8,16 @@ class SanctionController < ApplicationController
 
   post '/sanctions/show' do
     @resident = Resident.find_by_id(params[:id])
-    if !@resident.sanctions.detect{|sanction| sanction.name == params[:sanction][:name]}
+    if @resident.sanctions.detect{|sanction| sanction.name == params[:sanction][:name]} || params[:sanction][:name] == ""
+      erb :'/sanctions/show'
+    else
     @sanction = Sanction.create(params[:sanction])
     @resident.sanctions << @sanction
     @sanction.save
     @resident.save
   end
     erb :'/sanctions/show'
-   
+
   end
 
 
