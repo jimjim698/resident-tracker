fb7bbecd7fa068d70ae3278be3179fa0744bf23f
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-09 01:21:10 +0000

    created display pages for residents

--- app/controllers/resident_controller.rb
+++ app/controllers/resident_controller.rb
@@ -5,14 +5,21 @@ class ResidentController < ApplicationController
   end
 
   post '/residents/new' do
-    if Resident.all.detect {|resident| resident.name == params[:resident][:name]}
+    if Resident.all.detect {|resident| resident.name == params[:resident][:name]} || params[:resident][:name]=="" || params[:resident][:age] == "" || params[:resident][:room_number] == ""
+
       redirect '/residents/new'
     else
       @resident = Resident.create(params[:resident])
       @resident.manager_id = session[:user_id]
       @resident.save
       redirect "/managers/#{session[:user_id]}"
-    end 
+    end
+  end
+
+  get '/residents/:id' do
+    @resident = Resident.find_by_id(params[:id])
+    erb :'/residents/show'
+    
   end
 
 end
